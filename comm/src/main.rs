3a0c0be063e579077144002af74c1e5216c7d4cf
fedorbykov <fibykov@edu.hse.ru>
2022-09-20 15:57:27 +0300

    solve comm

--- comm/src/main.rs
+++ comm/src/main.rs
@@ -1,5 +1,34 @@
 #![forbid(unsafe_code)]
 
+use std::{
+    collections::HashSet,
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    // TODO: your code here.
+    let args = env::args().collect::<Vec<String>>();
+    let firstFile = std::fs::File::open(&args[1]).unwrap();
+    let mut reader = BufReader::new(firstFile);
+
+    let mut firstSet = HashSet::new();
+
+    for line in reader.lines() {
+        firstSet.insert(line.unwrap());
+    }
+
+    let secondFile = std::fs::File::open(&args[2]).unwrap();
+    reader = BufReader::new(secondFile);
+
+    let mut secondSet = HashSet::new();
+
+    for line in reader.lines() {
+        secondSet.insert(line.unwrap());
+    }
+
+    let mut intersection = firstSet.intersection(&secondSet);
+    for line in intersection {
+        println!("{}", line);
+    }
 }
