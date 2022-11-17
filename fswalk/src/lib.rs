f3d94cbc2947cfc85c7cee16b8fda600ed0d3312
fedorbykov <fibykov@edu.hse.ru>
2022-11-18 01:38:24 +0300

    Solved fswalk

--- fswalk/src/lib.rs
+++ fswalk/src/lib.rs
@@ -1,5 +1,6 @@
 #![forbid(unsafe_code)]
 
+use std::io::Read;
 use std::{fs, io, path::Path};
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -13,24 +14,85 @@ pub struct Walker<'a> {
 
 impl<'a> Walker<'a> {
     pub fn new() -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        Self {
+            callbacks: Vec::new(),
+        }
     }
 
     pub fn add_callback<F>(&mut self, callback: F)
     where
         F: FnMut(&mut Handle) + 'a,
     {
-        // TODO: your code here.
-        unimplemented!()
+        self.callbacks.push(Box::new(callback));
     }
 
     pub fn walk<P: AsRef<Path>>(&mut self, path: P) -> io::Result<()> {
-        // TODO: your code here.
-        unimplemented!()
+        if self.callbacks.is_empty() {
+            return Ok(());
+        }
+        Self::recursive_walk(path.as_ref(), self.callbacks.as_mut_slice())?;
+        Ok(())
     }
 
-    // TODO: your code here.
+    fn recursive_walk(dir: &Path, callbacks: &mut [Box<Callback>]) -> io::Result<()> {
+        if !dir.is_dir() {
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "directory not found",
+            ));
+        }
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            let mut handle: Handle = {
+                if path.is_file() {
+                    Handle::File(FileHandle {
+                        path: &path,
+                        flag: false,
+                    })
+                } else if path.is_dir() {
+                    Handle::Dir(DirHandle {
+                        path: &path,
+                        flag: false,
+                    })
+                } else {
+                    continue;
+                }
+            };
+            let mut anchor = 0;
+            for i in 0..callbacks.len() {
+                (callbacks[i])(&mut handle);
+                if was_checked(&mut handle) {
+                    if anchor < i {
+                        callbacks.swap(anchor, i);
+                    }
+                    anchor += 1;
+                }
+            }
+            if anchor == 0 {
+                continue;
+            }
+            match handle {
+                Handle::Dir(dir_handle) => {
+                    Self::recursive_walk(dir_handle.path(), &mut callbacks[0..anchor])?;
+                }
+                Handle::File(file_handle) => {
+                    let mut file = fs::File::open(file_handle.path())?;
+                    let mut buf: Vec<u8> = Vec::new();
+                    file.read_to_end(&mut buf)?;
+                    let mut content_handle = Handle::Content {
+                        file_path: file_handle.path(),
+                        content: &buf,
+                    };
+                    for callback in callbacks.iter_mut().take(anchor) {
+                        (callback)(&mut content_handle);
+                    }
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -44,36 +106,48 @@ pub enum Handle<'a> {
     },
 }
 
+pub fn was_checked(handle: &mut Handle) -> bool {
+    match handle {
+        Handle::Dir(dir) => {
+            let ans = dir.flag;
+            dir.flag = false;
+            ans
+        }
+        Handle::File(file) => {
+            let ans = file.flag;
+            file.flag = false;
+            ans
+        }
+        _ => false,
+    }
+}
+
 pub struct DirHandle<'a> {
     path: &'a Path,
-    // TODO: your code here.
+    flag: bool,
 }
 
 impl<'a> DirHandle<'a> {
     pub fn descend(&mut self) {
-        // TODO: your code here.
-        unimplemented!()
+        self.flag = true;
     }
 
     pub fn path(&self) -> &Path {
-        // TODO: your code here.
-        unimplemented!()
+        self.path
     }
 }
 
 pub struct FileHandle<'a> {
     path: &'a Path,
-    // TODO: your code here.
+    flag: bool,
 }
 
 impl<'a> FileHandle<'a> {
     pub fn read(&mut self) {
-        // TODO: your code here.
-        unimplemented!()
+        self.flag = true;
     }
 
     pub fn path(&self) -> &Path {
-        // TODO: your code here.
-        unimplemented!()
+        self.path
     }
 }
