f8479b19196b66ba5029ee585de2dc00894f48ca
fedorbykov <fibykov@edu.hse.ru>
2022-11-25 02:02:54 +0300

    Solved gc

--- gc/gc-derive/src/lib.rs
+++ gc/gc-derive/src/lib.rs
@@ -1,9 +1,32 @@
-use proc_macro::{Span, TokenStream};
-use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Ident};
+use proc_macro::{TokenStream};
+use quote::{quote};
 
 #[proc_macro_derive(Scan)]
 pub fn derive_scan(input: TokenStream) -> TokenStream {
-    // TODO: your code here.
-    unimplemented!()
+    let tokens = proc_macro2::TokenStream::from(input);
+    let mut iter = tokens.into_iter();
+    iter.next();
+    let Some(proc_macro2::TokenTree::Ident(ident)) = iter.next() else {
+        return TokenStream::new();
+    };
+    if let Some(proc_macro2::TokenTree::Group(group)) = iter.next() {
+        let field = group.stream().into_iter().next().unwrap();
+        let construct = quote! {
+            impl Scan for #ident {
+                fn get_allocations(&self) -> Vec<usize> {
+                    self.#field.get_allocations()
+                }
+            }
+        };
+        TokenStream::from(construct)
+    } else {
+        let construct = quote! {
+            impl Scan for #ident {
+                fn get_allocations(&self) -> Vec<usize> {
+                    vec![]
+                }
+            }
+        };
+        TokenStream::from(construct)
+    }
 }
--- gc/src/lib.rs
+++ gc/src/lib.rs
@@ -13,9 +13,14 @@ use std::{
 ////////////////////////////////////////////////////////////////////////////////
 
 pub struct Gc<T> {
-    weak: Weak<T>,
+    pub weak: Weak<T>,
 }
 
+impl<T> Gc<T> {
+    pub fn get_address(&self) -> usize {
+        self.weak.as_ptr() as usize
+    }
+}
 impl<T> Clone for Gc<T> {
     fn clone(&self) -> Self {
         Self {
@@ -26,8 +31,10 @@ impl<T> Clone for Gc<T> {
 
 impl<T> Gc<T> {
     pub fn borrow(&self) -> GcRef<'_, T> {
-        // TODO: your code here.
-        unimplemented!()
+        GcRef {
+            rc: self.weak.upgrade().unwrap(),
+            lifetime: PhantomData::<&'_ Gc<T>>,
+        }
     }
 }
 
@@ -47,40 +54,125 @@ impl<'a, T> Deref for GcRef<'a, T> {
 ////////////////////////////////////////////////////////////////////////////////
 
 pub trait Scan {
-    // TODO: your code here.
+    fn get_allocations(&self) -> Vec<usize>;
 }
 
-// TODO: your code here.
+impl<T: Scan + 'static> Scan for Gc<T> {
+    fn get_allocations(&self) -> Vec<usize> {
+        vec![self.get_address()]
+    }
+}
+
+impl<T: Scan + 'static> Scan for Vec<T> {
+    fn get_allocations(&self) -> Vec<usize> {
+        let mut answer: Vec<usize> = vec![];
+        for item in self.iter() {
+            answer.append(&mut item.get_allocations());
+        }
+        answer
+    }
+}
+
+impl<T: Scan + 'static> Scan for Option<T> {
+    fn get_allocations(&self) -> Vec<usize> {
+        if let Some(resource) = self.as_ref() {
+            return resource.get_allocations();
+        }
+        vec![]
+    }
+}
+
+impl<T: Scan + 'static> Scan for RefCell<T> {
+    fn get_allocations(&self) -> Vec<usize> {
+        self.borrow().get_allocations()
+    }
+}
+
+impl Scan for i32 {
+    fn get_allocations(&self) -> Vec<usize> {
+        vec![]
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
 pub struct Arena {
-    // TODO: your code here.
+    allocations: Vec<Rc<dyn Scan + 'static>>,
 }
 
 impl Arena {
     pub fn new() -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        Self {
+            allocations: Vec::<Rc<dyn Scan + 'static>>::new(),
+        }
     }
 
     pub fn allocation_count(&self) -> usize {
-        // TODO: your code here.
-        unimplemented!()
+        self.allocations.len()
     }
 
     pub fn alloc<T: Scan + 'static>(&mut self, obj: T) -> Gc<T> {
-        // TODO: your code here.
-        unimplemented!()
+        let rc: Rc<T> = Rc::new(obj);
+        let weak = Rc::<T>::downgrade(&rc);
+        self.allocations.push(rc);
+        Gc { weak }
     }
 
     pub fn sweep(&mut self) {
-        // TODO: your code here.
-        unimplemented!()
+        let index: HashMap<usize, usize> = (0..self.allocations.len())
+            .map(|i| (Rc::as_ptr(&self.allocations[i]) as *const u8 as usize, i))
+            .collect();
+
+        let mut cnt = vec![0; self.allocations.len()];
+        let graph: Vec<Vec<usize>> = self
+            .allocations
+            .iter()
+            .map(|alloc| {
+                alloc
+                    .get_allocations()
+                    .into_iter()
+                    .map(|x| {
+                        let ans = index[&x];
+                        cnt[ans] += 1;
+                        ans
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let mut marked = HashSet::<usize>::new();
+        for (i, links) in cnt.iter().enumerate() {
+            if Rc::weak_count(&self.allocations[i]) <= *links {
+                continue;
+            }
+            Self::mark_all(i, &mut marked, &graph);
+        }
+        let mut anchor = 0;
+        for i in 0..self.allocations.len() {
+            if marked.contains(&i) {
+                if i > anchor {
+                    self.allocations.swap(anchor, i);
+                }
+                anchor += 1;
+            }
+        }
+        self.allocations.truncate(anchor);
     }
 
-    fn mark_all(&self, root_addr: usize, marked: &mut HashSet<usize>) {
-        // TODO: your code here.
-        unimplemented!()
+    fn mark_all(root_addr: usize, marked: &mut HashSet<usize>, graph: &Vec<Vec<usize>>) {
+        //println!("Marking {}", root_addr);
+        //println!("{:?}", graph[root_addr]);
+        if !marked.insert(root_addr) {
+            return;
+        }
+        for u in graph[root_addr].iter() {
+            Self::mark_all(*u, marked, graph);
+        }
+    }
+}
+
+impl Default for Arena {
+    fn default() -> Self {
+        Self::new()
     }
 }
