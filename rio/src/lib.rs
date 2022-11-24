a395665e7a379362f504ca6ee9d42b7be84d8c87
Sergei Fomin <gh4exh3w62@fastmail.com>
2022-11-24 11:20:50 +0300

    Add rio

---
+++ rio/src/lib.rs
@@ -0,0 +1,10 @@
+mod network;
+mod runtime;
+mod scheduler;
+mod timer;
+
+pub use rio_macros::test;
+
+pub use network::UdpSocket;
+pub use runtime::{spawn, Runtime};
+pub use timer::sleep;
---
+++ rio/tests/test_timer.rs
@@ -0,0 +1,11 @@
+use std::time::{Duration, Instant};
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[rio::test]
+async fn test_simple() {
+    let start = Instant::now();
+    let duration = Duration::from_secs(1);
+    rio::sleep(duration).await;
+    assert!(start.elapsed() >= duration);
+}
