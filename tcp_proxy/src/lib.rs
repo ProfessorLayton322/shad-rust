4afcbc15f787a1b380d6ebe5bed7edce56c9bd46
fedorbykov <fibykov@edu.hse.ru>
2022-12-01 18:54:54 +0300

    Attempt on tcp_proxy

--- tcp_proxy/src/lib.rs
+++ tcp_proxy/src/lib.rs
@@ -3,10 +3,54 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-use log::info;
-use std::io::prelude::*;
+use std::borrow::Borrow;
+use std::io::copy;
+use std::io::Result;
+use std::sync::{Arc, Mutex};
+
+pub struct StreamWrapper {
+    inner: Arc<Mutex<TcpStream>>,
+}
+
+impl std::io::Read for StreamWrapper {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let mtx: &Mutex<TcpStream> = self.inner.borrow();
+        mtx.lock().unwrap().read(buf)
+    }
+}
+
+impl std::io::Write for StreamWrapper {
+    fn flush(&mut self) -> Result<()> {
+        let mtx: &Mutex<TcpStream> = self.inner.borrow();
+        mtx.lock().unwrap().flush()
+    }
+
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        let mtx: &Mutex<TcpStream> = self.inner.borrow();
+        mtx.lock().unwrap().write(buf)
+    }
+}
+
+pub fn two_arcs(stream: TcpStream) -> (StreamWrapper, StreamWrapper) {
+    let arc = Arc::new(Mutex::new(stream));
+    (
+        StreamWrapper { inner: arc.clone() },
+        StreamWrapper { inner: arc },
+    )
+}
 
 pub fn run_proxy(port: u32, destination: String) {
-    // TODO: your code here.
-    unimplemented!()
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    for incoming in listener.incoming() {
+        let client = incoming.unwrap();
+        let server = TcpStream::connect(destination.as_str()).unwrap();
+        let (mut client1, mut client2) = two_arcs(client);
+        let (mut server1, mut server2) = two_arcs(server);
+        thread::spawn(move || {
+            copy(&mut client1, &mut server1).unwrap();
+        });
+        thread::spawn(move || {
+            copy(&mut client2, &mut server2).unwrap();
+        });
+    }
 }
